9723e5171ea7abe7eb341cd8ea3194feb5cd6802
Keith McKenna <keithmckenna@gmail.com>
2020-03-06 01:35:36 +0000

    first commit to new repo - fully finished portfolio website

---
+++ routes/contact.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var router = express.Router();
+var nodemailer = require("nodemailer");
+
+// POST route from contact form
+router.post('/contact', (req, res) => {
+
+  // Instantiate the SMTP server
+  const smtpTrans = nodemailer.createTransport({
+    host: "smtp.stackmail.com",
+	port: 465,
+	secure: true,
+    auth: {
+    	user: "hello@keithmckenna.com",
+        pass: process.env.MAILEPW
+    }
+  })
+
+  // Specify what the email will look like
+  const mailOpts = {
+    from: '"CONTACT FORM ✉" <hello@keithmckenna.com>',
+    to: 'hello@keithmckenna.com',
+	replyTo: `${req.body.email}`,
+    subject: '❗ New Contact Form Submitted',
+    text: `From: ${req.body.name} (${req.body.email}) -
+			Subject: (${req.body.subject})
+			${req.body.name} says: ${req.body.message}`
+  }
+
+  // Attempt to send the email
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+        req.flash("error", "Sorry! Please try again, something went wrong.");
+        res.redirect("back");
+    }
+    else {
+        req.flash("success", "Contact form submitted successfully. Thanks for reaching out! I'll be in touch shortly.");
+        res.redirect("back");
+    }
+  })
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+var express         = require("express"),
+    app             = express(),
+	flash           = require("connect-flash"),
+    bodyParser      = require("body-parser");
+
+//require route
+var contactRoutes	= require("./routes/contact"),
+	indexRoutes		= require("./routes/index")
+	
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(flash());
+
+app.use(require("express-session")({
+    secret: "Dairy is the devil",
+    resave: false,
+    saveUninitialized: false 
+}));
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use(indexRoutes);
+app.use(contactRoutes);
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
